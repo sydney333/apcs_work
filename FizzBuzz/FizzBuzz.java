be1e8fc6c5803e6dc274351a17140002dc2b8841
sydneyadamson23 <89808069+sydneyadamson23@users.noreply.github.com>
2021-09-20 22:51:22 -0700

    geometric class

--- FizzBuzz/FizzBuzz.java
+++ FizzBuzz/FizzBuzz.java
@@ -3,16 +3,16 @@ public class FizzBuzz{
   public static void main(String[] args){
 
     for(int i = 0; i<30; i++){
-      if(i%3==i*3){
+      if(i%5==5){
         System.out.println("Fizz");
       }
-      else if(i%5==i*5){
+      if(i%3==3){
         System.out.println("Buzz");
       }
-      else if(i%2==i*3 && i%4==i*5){
+      if(i%3==3 && i%5==5){
         System.out.println("FizzBuzz");
       }
-      else{
+      else if{
         System.out.println(i);
       }
       }
