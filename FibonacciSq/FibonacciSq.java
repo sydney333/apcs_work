0ec77398ba5891b606b29981245024e56c74daa0
sydneyadamson23 <89808069+sydneyadamson23@users.noreply.github.com>
2021-09-20 23:52:24 -0700

    Fibonacci mid done

--- FibonacciSq/FibonacciSq.java
+++ FibonacciSq/FibonacciSq.java
@@ -1,8 +1,12 @@
 public class FibonacciSq{
 
   public static void main(String[] args){
+    int a = 1;
+    int b = 2;
+    int c = a+b;
+
       for(int i=0; i>20; i++){
-        System.out.println(i);
+        System.out.println(a + "," + b + "," + c);
       }
 
   }
