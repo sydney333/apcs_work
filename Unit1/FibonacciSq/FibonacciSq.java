7b5a6bff3534a575a294481c522edd087ca27a5c
sydneyadamson23 <89808069+sydneyadamson23@users.noreply.github.com>
2021-09-21 07:27:44 -0700

    update

--- Unit1/FibonacciSq/FibonacciSq.java
+++ Unit1/FibonacciSq/FibonacciSq.java
@@ -1,5 +1,8 @@
 public class FibonacciSq{
-
+//code isn't working, not displaying in Terminal
+//tried using Cat to see if it displayed my code and it did
+//pushed to origin, used javac repeatedly, and was saving my work --> all with no luck
+//I would like to trouble shoot this if you have time!
   public static void main(String[] args){
     int a = 1;
     int b = 2;
